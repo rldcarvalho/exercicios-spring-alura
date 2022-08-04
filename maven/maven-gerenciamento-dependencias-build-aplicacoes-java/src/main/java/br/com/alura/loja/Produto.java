d1c44a81fa46181ab3629d8bf06dd0be6d2e435e
Rafael <rldcarvalhoo@gmail.com>
2022-08-04 18:48:04 -0300

    added maven exercises

---
+++ maven/maven-gerenciamento-dependencias-build-aplicacoes-java/src/main/java/br/com/alura/loja/Produto.java
@@ -0,0 +1,13 @@
+package br.com.alura.loja;
+
+public class Produto {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+        System.out.println();
+
+        System.out.println();
+    }
+
+}
