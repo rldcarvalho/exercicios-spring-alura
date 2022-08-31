b470f4c26e5ccc742e8751796246ed83317e9ff1
Rafael <rldcarvalhoo@gmail.com>
2022-08-31 18:34:49 -0300

    autorization and logout system

--- Servlet/src/main/java/br/com/rldcarvalho/gerenciador/acao/ListaEmpresas.java
+++ Servlet/src/main/java/br/com/rldcarvalho/gerenciador/acao/ListaEmpresas.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.rldcarvalho.gerenciador.modelo.Banco;
 import br.com.rldcarvalho.gerenciador.modelo.Empresa;
--- Servlet/src/main/webapp/WEB-INF/view/listaEmpresas.jsp
+++ Servlet/src/main/webapp/WEB-INF/view/listaEmpresas.jsp
@@ -11,6 +11,14 @@
 <title>Java Standard Taglib</title>
 </head>
 <body>
+
+	<c:import url="logout-parcial.jsp" />
+	
+	Usuario Logado: ${usuarioLogado.login }
+	
+	<br>
+	<br>
+
 	<c:if test="${not empty empresa }">
 		Empresa ${ empresa } cadastrada com sucesso
 	</c:if>
