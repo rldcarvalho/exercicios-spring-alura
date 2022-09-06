f3fc7e299a2f597b14e3d9f287c08a1ee1338b37
Rafael <rldcarvalhoo@gmail.com>
2022-09-06 17:28:13 -0300

    pool connection

--- jdbc/loja-virtual-repository/src/main/java/br/com/rldcarvalho/db/ConnectionFactory.java
+++ jdbc/loja-virtual-repository/src/main/java/br/com/rldcarvalho/db/ConnectionFactory.java
@@ -1,23 +1,32 @@
 package br.com.rldcarvalho.db;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.DataSources;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public Connection recuperarConexao() {
+    static String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
+    static String user = "root";
+    static String password = "password";
 
-        String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
-        String user = "root";
-        String password = "password";
+    public DataSource dataSource;
 
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
+    public ConnectionFactory(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl(url);
+        comboPooledDataSource.setUser(user);
+        comboPooledDataSource.setPassword(password);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection recuperarConexao() throws SQLException {
+
+        return dataSource.getConnection();
     }
 }
