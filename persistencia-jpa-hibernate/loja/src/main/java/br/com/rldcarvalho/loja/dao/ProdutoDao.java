be9d1f3788c5972e27ab134ac4db042c19037cbe
Rafael <rldcarvalhoo@gmail.com>
2022-09-09 21:02:49 -0300

    Criteria API

--- persistencia-jpa-hibernate/loja/src/main/java/br/com/rldcarvalho/loja/dao/ProdutoDao.java
+++ persistencia-jpa-hibernate/loja/src/main/java/br/com/rldcarvalho/loja/dao/ProdutoDao.java
@@ -3,7 +3,14 @@ package br.com.rldcarvalho.loja.dao;
 import br.com.rldcarvalho.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ProdutoDao {
@@ -40,5 +47,53 @@ public class ProdutoDao {
     public BigDecimal buscarPrecoDoProdutoComNome(String nome){
         String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
         return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult();
+    }
+
+    public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro){
+        String jpql = "SELECT p FROM Produto p WHERE 1=1";
+        if (nome != null && !nome.trim().isEmpty()){
+            jpql = "AND p.nome = :nome ";
+        }
+        if (preco != null){
+            jpql = "AND p.preco = :preco ";
+        }
+        if (dataCadastro != null){
+            jpql = "AND p.dataCadastro = :dataCadastro ";
+        }
+
+        TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+        if (nome != null && !nome.trim().isEmpty()){
+            query.setParameter("nome", nome);
+        }
+        if (preco != null){
+            query.setParameter("preco", preco);
+        }
+        if (dataCadastro != null){
+            query.setParameter("dataCadastro", dataCadastro);
+        }
+
+        return query.getResultList();
+    }
+
+    public List<Produto> buscarPorParametrosComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+        Root<Produto> from = query.from(Produto.class);
+
+        Predicate filtros = builder.and();
+        if (nome != null && !nome.trim().isEmpty()){
+            filtros = builder.and(filtros, builder.equal(from.get("nome"), nome));
+        }
+        if (preco != null){
+            filtros = builder.and(filtros, builder.equal(from.get("preco"), preco));
+        }
+        if (dataCadastro != null){
+            filtros = builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
+        }
+
+        query.where(filtros);
+
+        return em.createQuery(query).getResultList();
+
     }
 }
