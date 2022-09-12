923d05e0d133ed6224ae0f43f3234e0e3c61d280
Rafael <rldcarvalhoo@gmail.com>
2022-09-12 17:40:39 -0300

    function update

--- data-jpa/spring-data/src/main/java/br/com/rldcarvalho/spring/data/service/CrudCargoService.java
+++ data-jpa/spring-data/src/main/java/br/com/rldcarvalho/spring/data/service/CrudCargoService.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
 
+    private Boolean system = true;
     private final CargoRepository cargoRepository;
 
     public CrudCargoService(CargoRepository cargoRepository){
@@ -16,6 +17,28 @@ public class CrudCargoService {
     }
 
     public void inicial(Scanner scanner){
+        while (system){
+            System.out.println("Qual ação de cargo deseja executar?");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+
+            int action = scanner.nextInt();
+
+            switch (action){
+
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
+
         salvar(scanner);
     }
 
@@ -28,4 +51,17 @@ public class CrudCargoService {
         System.out.println("Salvo");
     }
 
+    private void atualizar(Scanner scanner){
+        System.out.println("Id");
+        int id = scanner.nextInt();
+        System.out.println("Descrição do Cargo");
+        String descricao = scanner.next();
+
+        Cargo cargo = new Cargo();
+        cargo.setId((long)id);
+        cargo.setDescricao(descricao);
+
+        cargoRepository.save(cargo);
+        System.out.println("Atualizado");
+    }
 }
