0d7ad3e4b1d8afc0897f0574a8a32acacfbe6758
Rafael <rldcarvalhoo@gmail.com>
2022-09-15 19:38:20 -0300

    saving new Pedido in Db

--- mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/controller/PedidoController.java
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,33 @@
 package br.com.rldcarvalho.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.rldcarvalho.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.rldcarvalho.mvc.mudi.model.Pedido;
+import br.com.rldcarvalho.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
 		return "pedido/formulario";
 	}
 
