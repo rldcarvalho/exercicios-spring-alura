4ff217f3b1424bf626c9c7cd5c26f86de607dad1
Rafael <rldcarvalhoo@gmail.com>
2022-09-19 15:18:25 -0300

    basic login config

---
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package br.com.rldcarvalho.mvc.mudi;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> {
+				try {
+					requests
+//				.antMatchers("/", "/home").permitAll()
+						.anyRequest().authenticated()
+						.and().httpBasic();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			);
+//			.formLogin((form) -> form
+//				.loginPage("/login")
+//				.permitAll()
+//			)
+//			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		@SuppressWarnings("deprecation")
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("rafael")
+				.password("12345")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
