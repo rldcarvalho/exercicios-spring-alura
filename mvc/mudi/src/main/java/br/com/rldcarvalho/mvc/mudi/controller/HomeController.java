9747220307c9dbc02033dd4b6ae93eea1081ae92
Rafael <rldcarvalhoo@gmail.com>
2022-09-20 19:26:15 -0300

    Sort and Page request

--- mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/controller/HomeController.java
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,7 +27,10 @@ public class HomeController {
 	@GetMapping
 	public String home(Model model, Principal principal) {
 	
-		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE);
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+		
+		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE, paginacao);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
