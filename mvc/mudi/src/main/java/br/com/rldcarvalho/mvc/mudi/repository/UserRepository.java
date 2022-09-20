919178d0be8d8f79d11c1a712192811ee744ac92
Rafael <rldcarvalhoo@gmail.com>
2022-09-20 00:00:51 -0300

    Associating User with the Pedido

---
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.rldcarvalho.mvc.mudi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import br.com.rldcarvalho.mvc.mudi.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+	
+	User findByUsername(String username);
+
+}
