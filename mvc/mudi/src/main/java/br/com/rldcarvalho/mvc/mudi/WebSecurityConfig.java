581fc79396b915a8cfa9403da1106de6662d5647
Rafael <rldcarvalhoo@gmail.com>
2022-09-19 22:47:36 -0300

    JDBC Authentication

--- mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/WebSecurityConfig.java
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/WebSecurityConfig.java
@@ -1,46 +1,53 @@
 package br.com.rldcarvalho.mvc.mudi;
 
+import javax.sql.DataSource;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
 		http
-			.authorizeHttpRequests((requests) -> requests
-//				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
-			)
-			.logout((logout) -> logout.logoutUrl("/logout"));
+		.authorizeRequests()
+//			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin((form) -> form
+			.loginPage("/login")
+			.defaultSuccessUrl("/home", true)
+			.permitAll()
+		)
+		.logout((logout) -> logout.logoutUrl("/logout"));
 
-		return http.build();
 	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		@SuppressWarnings("deprecation")
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("rafael")
-				.password("12345")
-				.roles("ADM")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+		
+//		UserDetails user = User.builder()
+//				.username("admin")
+//				.password(encoder.encode("12345"))
+//				.roles("ADM")
+//				.build();
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
+//			.withUser(user);
 	}
 }
