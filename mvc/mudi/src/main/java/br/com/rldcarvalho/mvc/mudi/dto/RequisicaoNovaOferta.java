03eaf26558460ca67c6501fd8c9affeab8f828f9
Rafael <rldcarvalhoo@gmail.com>
2022-09-26 20:26:23 -0300

    applying bean validation in Oferta inputs

--- mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ mvc/mudi/src/main/java/br/com/rldcarvalho/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import br.com.rldcarvalho.mvc.mudi.model.Oferta;
 
 public class RequisicaoNovaOferta {
@@ -12,8 +15,12 @@ public class RequisicaoNovaOferta {
 	
 	private Long pedidoId;
 	
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")
+	@NotEmpty
 	private String valor;
 	
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
+	@NotEmpty
 	private String dataDaEntrega;
 	
 	private String comentario;
