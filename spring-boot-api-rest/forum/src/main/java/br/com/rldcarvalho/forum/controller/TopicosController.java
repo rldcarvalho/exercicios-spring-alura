e73887a88e054e04ffba1991190429caf1008891
Rafael <rldcarvalhoo@gmail.com>
2022-09-27 17:53:42 -0300

    invalidating cache

--- spring-boot-api-rest/forum/src/main/java/br/com/rldcarvalho/forum/controller/TopicosController.java
+++ spring-boot-api-rest/forum/src/main/java/br/com/rldcarvalho/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import br.com.rldcarvalho.forum.modelo.Topico;
 import br.com.rldcarvalho.forum.repository.CursoRepository;
 import br.com.rldcarvalho.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -48,6 +49,7 @@ public class TopicosController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){
         Topico topico =  form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -68,6 +70,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
         Optional<Topico> optional = topicoRepository.findById(id);
         if(optional.isPresent()){
@@ -80,6 +83,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id){
         Optional<Topico> optional = topicoRepository.findById(id);
         if(optional.isPresent()){
