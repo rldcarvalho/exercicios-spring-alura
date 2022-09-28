039556a7c25068af331bc32bb663efc6c6568ee0
Rafael <rldcarvalhoo@gmail.com>
2022-09-28 18:16:33 -0300

    Returning the token to the client

--- spring-boot-api-rest/forum/src/main/java/br/com/rldcarvalho/forum/controller/AutenticacaoController.java
+++ spring-boot-api-rest/forum/src/main/java/br/com/rldcarvalho/forum/controller/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package br.com.rldcarvalho.forum.controller;
 
 import br.com.rldcarvalho.forum.config.security.TokenService;
+import br.com.rldcarvalho.forum.controller.dto.TokenDto;
 import br.com.rldcarvalho.forum.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -29,16 +30,15 @@ public class AutenticacaoController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form){
         UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
         try {
             Authentication authentication = authManager.authenticate(dadosLogin);
 
             String token = tokenService.gerarToken(authentication);
-            System.out.println(token);
 
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
         }catch (AuthenticationException e){
             return ResponseEntity.badRequest().build();
         }
