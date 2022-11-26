ef6f593ca4b74895adde6ea91ccefc37c79f966b
Rafael <rldcarvalhoo@gmail.com>
2022-11-25 22:16:24 -0300

    Added model and repository classes

---
+++ spring-boot-3/api/src/main/java/med/voll/api/model/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.controller.dto.DadosEnderecoDto;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEnderecoDto dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
+}
